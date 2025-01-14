044e8b345647eea66a7168ebcfc76ab8a066cf4c
appoclayptor <shahreza1414@gmail.com>
2025-01-14 22:20:53 +0600

    enhance Banner component with scroll-based button styling; redesign Login page layout and add Google sign-in functionality

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import freelancer from '../../../assets/banner/whitehairwoman.mp4'
 
 const Banner = () => {
+   const [isScroll, setIsScroll] = useState(false);
+   useEffect(() => {
+      window.addEventListener('scroll', () => {
+         if (scrollY > 80) {
+            setIsScroll(true)
+         } else {
+            setIsScroll(false)
+         }
+      })
+   }, []);
    return (
       <div className='relative w-full h-svh md:h-[700px]'>
          {/* video */}
@@ -13,7 +23,7 @@ const Banner = () => {
             <p className='text-lg text-primary-light'>Complete Tasks, Earn Rewards</p>
             <h2 className='text-6xl max-w-4xl leading'>Maximize Productivity With <br /> <span className='font-surfer font-semibold text-primary-dark'>Freelance Marketplace</span> <br />Solution</h2>
             <div>
-               <button className='btn bg-primary-light border-none px-9'>Get Started</button>
+               <button className={isScroll ? 'btn bg-secondary border-none px-9' : 'btn bg-primary-light border-none px-9'}>Get Started</button>
             </div>
          </div>
       </div>
