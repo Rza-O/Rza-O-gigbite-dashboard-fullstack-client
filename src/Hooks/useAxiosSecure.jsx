8fc16a149426502dc9c006f88190ccbbb376c941
appoclayptor <shahreza1414@gmail.com>
2025-01-16 04:15:06 +0600

    implement axios interceptors for secure token handling; add logout and navigation on 401/403 responses

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,18 +1,32 @@
 import axios from 'axios';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
    baseURL: 'http://localhost:8000'
 })
 
 const useAxiosSecure = () => {
+   const { logOut } = useAuth();
+   const navigate = useNavigate();
    axiosInstance.interceptors.request.use((config) => {
       const token = localStorage.getItem('access-token');
-      console.log('this is intercepting from interceptors console->', token)
       config.headers.authorization = `Bearer ${token}`;
       return config;
    }, (error) => {
       return Promise.reject(error);
    });
+
+   axiosInstance.interceptors.response.use((response) => {
+      return response;
+   }, async (error) => {
+      const status = error.response?.status;
+      if (status === 401 || status === 403) {
+         await logOut();
+         navigate('/login')
+      }
+      return Promise.reject(error);
+   })
    return axiosInstance;
 };
 
