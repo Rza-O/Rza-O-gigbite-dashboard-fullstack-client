bcbb9021b8362be7ec6efbbb1b661772fbe1e932
appoclayptor <shahreza1414@gmail.com>
2025-01-18 07:04:54 +0600

    add WithdrawRequest component; implement withdrawal request retrieval and approval functionality, enhance AdminHome layout with new component

---
+++ src/Components/Dashboard/Admin/Home/WithdrawRequest.jsx
@@ -0,0 +1,74 @@
+import Loading from '@/Components/Shared/LoadingSpinner/Loading';
+import useAxiosSecure from '@/Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import toast from 'react-hot-toast';
+
+const WithdrawRequest = ({ refetchStats }) => {
+   const axiosSecure = useAxiosSecure();
+   const { data: withdrawRequests, isLoading, refetch } = useQuery({
+      queryKey: ['withdrawRequest'],
+      queryFn: async () => {
+         const { data } = await axiosSecure('/admin/withdrawal-requests');
+         return data;
+      }
+   })
+   console.log(withdrawRequests);
+   if (isLoading) return <Loading></Loading>
+
+   const handleApproval = async (id) => {
+      try {
+         const { data } = await axiosSecure.patch(`/admin/approval/${id}`)
+         console.log(data);
+         refetchStats();
+         refetch();
+         toast.success('Withdrawal request approved')
+      } catch (error) {
+         console.log(error);
+         toast.error('Approval failed. Please try again!')
+      }
+   }
+
+
+
+   return (
+      <div>
+         {withdrawRequests.length > 0 ? (
+            <div className="overflow-x-auto ">
+               <table className="table static">
+                  {/* head */}
+                  <thead>
+                     <tr>
+                        <th>#</th>
+                        <th>Worker</th>
+                        <th>Coin</th>
+                        <th>Action</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {/* row 1 */}
+                     {withdrawRequests.map((request, idx) => (
+                        <tr key={request._id} className="hover:bg-primary/30 border-border">
+                           <th>{idx + 1}</th>
+                           <td>{request?.worker_name}</td>
+                           <td>{request?.withdrawal_coin}🪙</td>
+                           <td>
+                              <button
+                                 onClick={() => handleApproval(request?._id)}
+                                 className='btn btn-success'>Approve</button>
+                           </td>
+                        </tr>
+                     ))}
+                  </tbody>
+               </table>
+            </div>
+         ) : (
+            <div>
+               <p className='text-center text-2xl'>There's no withdrawal request yet</p>
+            </div>
+         )}
+      </div>
+   );
+};
+
+export default WithdrawRequest;
--- src/Pages/Dashboard/Admin/Home/AdminHome.jsx
+++ src/Pages/Dashboard/Admin/Home/AdminHome.jsx
@@ -1,4 +1,6 @@
 import AdminStats from '@/Components/Dashboard/Admin/Home/AdminStats';
+import WithdrawRequest from '@/Components/Dashboard/Admin/Home/WithdrawRequest';
+import DashboardTitle from '@/Components/Dashboard/Shared/Title/DashboardTitle';
 import Loading from '@/Components/Shared/LoadingSpinner/Loading';
 import useAxiosSecure from '@/Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
@@ -6,7 +8,7 @@ import React from 'react';
 
 const AdminHome = () => {
    const axiosSecure = useAxiosSecure();
-   const { data: stats, isLoading: statsLoading, refetch } = useQuery({
+   const { data: stats, isLoading: statsLoading, refetch: refetchStats } = useQuery({
       queryKey: ['stats'],
       queryFn: async () => {
          const { data } = await axiosSecure('/admin-stats');
@@ -15,8 +17,16 @@ const AdminHome = () => {
    })
    if (statsLoading) return <Loading></Loading>
    return (
-      <div>
-         <AdminStats stats={stats}></AdminStats>
+      <div className='w-11/12 mx-auto my-6'>
+         <div className='container mx-auto'>
+            <AdminStats stats={stats}></AdminStats>
+         </div>
+         <div className='my-6'>
+            <DashboardTitle title={'Withdrawals Request'} subtitle={'Approve withdrawals for the worker'}></DashboardTitle>
+         </div>
+         <div className='container mx-auto'>
+            <WithdrawRequest refetchStats={refetchStats}></WithdrawRequest>
+         </div>
       </div>
    );
 };
