a10e0f96357a04b7fab6640bf5e5926c8c435602
appoclayptor <shahreza1414@gmail.com>
2025-01-18 10:14:39 +0600

    refactor MySubmissionsTable; implement pagination for submissions, enhance data retrieval with current page state, and improve table layout

--- src/Components/Dashboard/Admin/ManageUsers/ManageUsersTable.jsx
+++ src/Components/Dashboard/Admin/ManageUsers/ManageUsersTable.jsx
@@ -44,7 +44,6 @@ const ManageUsersTable = () => {
 
    return (
       <div className="container mx-auto p-6">
-         <h1 className="text-2xl font-bold mb-4">Manage Users</h1>
          <div className="overflow-x-auto">
             <table className="table w-full static">
                {/* Table Head */}
--- src/Components/Dashboard/Worker/MySubmissionsTable/MySubmissionsTable.jsx
+++ src/Components/Dashboard/Worker/MySubmissionsTable/MySubmissionsTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MySubmissionsTableRow from './MySubmissionsTableRow';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '@/Hooks/useAuth';
@@ -8,41 +8,68 @@ import Loading from '@/Components/Shared/LoadingSpinner/Loading';
 const MySubmissionsTable = () => {
    const { user, loading } = useAuth();
    const axiosSecure = useAxiosSecure();
+   const [currentPage, setCurrentPage] = useState(1);
+   const itemsPerPage = 5;
 
-   const { data: mySubmissions = [], isLoading } = useQuery({
-      queryKey: ['mySubmissions'],
-      enabled: !loading && !! user?.email,
+   const { data = {}, isLoading } = useQuery({
+      queryKey: ['mySubmissions', currentPage],
+      enabled: !loading && !!user?.email,
       queryFn: async () => {
-         const { data } = await axiosSecure(`/my-submissions/${user?.email}`)
+         const { data } = await axiosSecure(`/my-submissions/${user?.email}?page=${currentPage}&limit=${itemsPerPage}`)
          return data;
       }
    })
 
-   if(isLoading) return <Loading></Loading>
+   const { submissions = [], totalPages } = data;
 
-   console.log(mySubmissions);
+   const handlePageChange = async (page) => {
+      setCurrentPage(page)
+   }
+
+   if (isLoading) return <Loading></Loading>
+
+   console.log(submissions)
 
 
    return (
-      <div className="overflow-x-auto">
-         <table className="table static">
-            {/* head */}
-            <thead>
-               <tr>
-                  <th>#</th>
-                  <th>Task Title</th>
-                  <th>Submission Date</th>
-                  <th>Status</th>
-               </tr>
-            </thead>
-            <tbody>
-               {/* row  */}
-               {
-                  mySubmissions.map((mySubmission, idx) => <MySubmissionsTableRow idx={idx} mySubmission={mySubmission} key={mySubmission._id}></MySubmissionsTableRow>)
-               }
-               
-            </tbody>
-         </table>
+      <div>
+         <div className="overflow-x-auto h-[300px]">
+            <table className="table static ">
+               {/* head */}
+               <thead>
+                  <tr>
+                     <th>#</th>
+                     <th>Task Title</th>
+                     <th>Submission Date</th>
+                     <th>Status</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {/* row  */}
+                  {
+                     submissions.map((mySubmission, idx) => <MySubmissionsTableRow idx={idx +(currentPage - 1) * itemsPerPage} mySubmission={mySubmission} key={mySubmission._id}></MySubmissionsTableRow>)
+                  }
+               </tbody>
+            </table>
+         
+         </div>
+         {/* pagination */}
+         <div className="flex justify-center mt-4">
+            <div className="join">
+               {Array.from({ length: totalPages }, (_, idx) => (
+                  <input
+                     key={idx}
+                     type="radio"
+                     name="pagination"
+                     aria-label={`Page ${idx + 1}`}
+                     className={`join-item btn bg-secondary  ${currentPage === idx + 1 ? 'btn-info' : ''
+                        }`}
+                     onClick={() => handlePageChange(idx + 1)}
+                     defaultChecked={currentPage === idx + 1}
+                  />
+               ))}
+            </div>
+         </div>
       </div>
    );
 };
